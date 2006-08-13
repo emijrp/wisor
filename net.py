af52719c7394da3ae92a7a4c9688747bd7b09283
yrithinnd <roberto@cebollitas.org>
2006-08-13 11:18:06 +0000

    Arreglo errores de unicode y error de fetch con simbolos raros y espacios. -Ahora pageText y fetch devuelven un unicode -Fetch, transforma pagina a una direccion web

--- net.py
+++ net.py
@@ -8,14 +8,15 @@ def pageText(url):
     request=urllib2.Request(url)
     user_agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7'
     request.add_header("User-Agent", user_agent)
+    request.add_header("Accept-Charset", 'utf-8')
     response=urllib2.urlopen(request)
     text=response.read()
     response.close()
-    return text
+    return unicode(text,'utf-8')
 
 def trurl(userinput):
 	return urllib.quote(userinput.encode('utf-8', 'replace'))
 
 def fetch(pagina,project='es',family='wikipedia'):
-	raw=pageText('http://'+project+'.'+family+'.org/w/index.php?title='+pagina+'&action=raw')
+	raw=pageText('http://'+project+'.'+family+'.org/w/index.php?title='+trurl(pagina)+'&action=raw')
 	return raw
